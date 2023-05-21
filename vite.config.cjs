c772539e474c616ed1f47c27826db2ae4ae108c7
ALEXIS404 <est.asanchezc@uml.edi.ni>
2023-05-20 18:04:53 -0600

    vite

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,17 +1,25 @@
 import { defineConfig } from "vite";
 const { resolve } = require('path')
+//Add env variable to check if is production or development
+
 const env = process.env;
+//Set a base route if deploying on github pages
+//env.mode = env.mode || "production";
 env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode);
+
 export default defineConfig({
   root: "./",
-
-  base: env.mode === "production" ? "/examen-renta/" : "/",
+  //Add base route if deployiing on production
+  base: env.mode === "production" ? "/app/" : "/",
   build: {
-    target: "esnext", //para compatibilidad de navegadores
+    target: "esnext",
     outDir: "docs",
     assetsDir: "assets",
     rollupOptions: {
       input: {
+        main: resolve(__dirname, 'index.html')
       }
     }
   },
